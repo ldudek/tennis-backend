32b6ab8bf9b07a4c0615d65738bd71d6ec7e2a4a
ldudek <luukasz.dudek@gmail.com>
2019-02-25 22:12:05 +0100

    MatchResult added

--- src/main/java/tennis/domain/player/PlayerController.java
+++ src/main/java/tennis/domain/player/PlayerController.java
@@ -1,8 +1,7 @@
 package tennis.domain.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,9 +11,46 @@ public class PlayerController {
     @Autowired
     PlayerService playerService;
 
-    @RequestMapping("/players")
-    public List<Player> listAllPlayers() {
+    @GetMapping("/players")
+    List<Player> all() {
         return playerService.listAll();
+    }
+
+    @PostMapping("/players")
+    Player addPlayer(@RequestBody Player player) {
+        return playerService.add(player);
+    }
+
+    @GetMapping("/players/{id}")
+    Player one(@PathVariable Long id) {
+
+        //TODO
+        return null;
+    }
+
+    @PutMapping("/players/{id}")
+    Player updatePlayer(@RequestBody Player newPlayer, @PathVariable Long id) {
+
+        //TODO
+        /*
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newEmployee.setId(id);
+                    return repository.save(newEmployee);
+                });
+        */
+        return null;
+    }
+
+
+    @DeleteMapping("/players/{id}")
+    void deletePlayer(@PathVariable Long id) {
+        //TODO
     }
 
 }
